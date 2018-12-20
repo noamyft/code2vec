d6c48a38639e802ed0263a2d29901a321c34eed0
noamyft <noamyft@gmail.com>
2018-12-20 19:47:03 +0200

    add preprocess_aug.py (like preprocess_aug but parallel)

---
+++ preprocess_aug.py
@@ -0,0 +1,37 @@
+import multiprocessing as mp
+import os
+import sys
+
+PYTHON="python3"
+EXTRACTOR="JavaExtractor/extract.py"
+NUM_THREADS=4
+REMOVE="rm"
+# REMOVE="del"
+
+def preprocess_dir(path:str):
+    # analysize both
+    os.system(PYTHON + " " + EXTRACTOR + " --dir " + path + " --max_path_length 8 --max_path_width 2 --num_threads " + str(NUM_THREADS) +
+              " --jar JavaExtractor/JPredict/target/JavaExtractor-0.0.1-SNAPSHOT.jar > " + path + "/augtempboth")
+    os.system(PYTHON + " preprocess_test_batch.py --test_data " +
+              path + "/augtempboth --max_contexts 200 --dict_file data/java14m/java14m --output_name " + path + "/both")
+    os.remove(path + "/augtempboth")
+
+    # analysize original
+    path = path
+    for f in os.listdir(path + "/src"):
+        if f[-13: -5] != "_mutants":
+            f = os.path.join(path,"src",f)
+            os.system(PYTHON + " " + EXTRACTOR + " --file " + f + " --max_path_length 8 --max_path_width 2 --num_threads " + str(NUM_THREADS) +
+                " --jar JavaExtractor/JPredict/target/JavaExtractor-0.0.1-SNAPSHOT.jar > " + path + "/augtemporigin")
+            os.system(PYTHON + " preprocess_test_batch.py --test_data " +
+                      path + "/augtemporigin --max_contexts 200 --dict_file data/java14m/java14m --output_name " + path + "/original")
+            os.remove(path + "/augtemporigin")
+
+
+if __name__ == "__main__":
+
+    mypath = sys.argv[1]
+    alldirs = [os.path.join(mypath, f).replace("\\", "/") for f in os.listdir(mypath) if os.path.isdir(os.path.join(mypath, f))]
+
+    pool = mp.Pool(processes=mp.cpu_count())
+    pool.map(preprocess_dir, alldirs)
