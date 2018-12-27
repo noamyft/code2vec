f9037360b959f2484931fb8afcbcf52e4fd218fe
noamyft <noamyft@gmail.com>
2018-12-27 16:10:15 +0200

    change preprocess to process original and mutants

--- preprocess_aug.py
+++ preprocess_aug.py
@@ -10,26 +10,28 @@ REMOVE="rm"
 # REMOVE="del"
 
 def preprocess_dir(path:str):
-    # analysize both
-    if (not CONTINUE_PROCESS) or (not os.path.exists(os.path.join(path,"both.test.c2v"))):
-        os.system(PYTHON + " " + EXTRACTOR + " --dir " + path + " --max_path_length 8 --max_path_width 2 --num_threads " + str(NUM_THREADS) +
-                  " --jar JavaExtractor/JPredict/target/JavaExtractor-0.0.1-SNAPSHOT.jar > " + path + "/augtempboth")
-        os.system(PYTHON + " preprocess_test_batch.py --test_data " +
-                  path + "/augtempboth --max_contexts 200 --dict_file data/java14m/java14m --output_name " + path + "/both")
-        os.remove(path + "/augtempboth")
 
-    # analysize original
-    if (not CONTINUE_PROCESS) or (not os.path.exists(os.path.join(path, "original.test.c2v"))):
-        path = path
-        for f in os.listdir(path + "/src"):
-            if f[-13: -5] != "_mutants":
-                f = os.path.join(path,"src",f)
-                os.system(PYTHON + " " + EXTRACTOR + " --file " + f + " --max_path_length 8 --max_path_width 2 --num_threads " + str(NUM_THREADS) +
+    for f in os.listdir(path + "/src"):
+        if f[-13: -5] != "_mutants": # analyize original
+            if (not CONTINUE_PROCESS) or (not os.path.exists(os.path.join(path, "original.test.c2v"))):
+                f = os.path.join(path, "src", f)
+                os.system(
+                    PYTHON + " " + EXTRACTOR + " --file " + f + " --max_path_length 8 --max_path_width 2 --num_threads " + str(
+                        NUM_THREADS) +
                     " --jar JavaExtractor/JPredict/target/JavaExtractor-0.0.1-SNAPSHOT.jar > " + path + "/augtemporigin")
                 os.system(PYTHON + " preprocess_test_batch.py --test_data " +
                           path + "/augtemporigin --max_contexts 200 --dict_file data/java14m/java14m --output_name " + path + "/original")
                 os.remove(path + "/augtemporigin")
-
+        else: # analyize mutants
+            if (not CONTINUE_PROCESS) or (not os.path.exists(os.path.join(path, "mutant.test.c2v"))):
+                f = os.path.join(path, "src", f)
+                os.system(
+                    PYTHON + " " + EXTRACTOR + " --file " + f + " --max_path_length 8 --max_path_width 2 --num_threads " + str(
+                        NUM_THREADS) +
+                    " --jar JavaExtractor/JPredict/target/JavaExtractor-0.0.1-SNAPSHOT.jar > " + path + "/augtempmut")
+                os.system(PYTHON + " preprocess_test_batch.py --test_data " +
+                          path + "/augtempmut --max_contexts 200 --dict_file data/java14m/java14m --output_name " + path + "/mutant")
+                os.remove(path + "/augtempmut")
 
 if __name__ == "__main__":
 
