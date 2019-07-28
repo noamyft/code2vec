4ea788055562785d2277a5f876a6820b9e47649a
Noam Yefet <snyefet@csf.technion.ac.il>
2019-07-29 02:00:54 +0300

    optimize adversarial searcher: do not derive nodes in the deepest level

--- adversarialsearcher.py
+++ adversarialsearcher.py
@@ -136,7 +136,10 @@ class AdversarialSearcher():
 
         new_nodes = {state: self._create_bfs_node(state, new_level, score) for state, score in new_valid_states}
         self.unchecked_nodes += new_nodes.values()
-        self.open_state_to_node.update(new_nodes)
+        if new_level < self.max_depth:
+            self.open_state_to_node.update(new_nodes)
+        else:
+            self.close_state_to_node.update(new_nodes)
 
 
     def _create_states(self, state, model_results, topk):
